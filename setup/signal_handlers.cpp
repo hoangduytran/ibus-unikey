0b206edbf29197939c42e733b5dc3bd1af4899b2
hoangduytran <hoangduytran1960@googlemail.com>
2026-04-05 22:19:59 +0100

    Add signal handler code comments for setup signal handlers

--- setup/signal_handlers.cpp
+++ setup/signal_handlers.cpp
@@ -2,12 +2,15 @@
 #include "controller/setup_controller.h"
 #include "unikey_config.h"
 
-void on_main_window_destroy(GtkWidget* w, gpointer user_data)
+// Bridge GTK main window destroy signal to the setup controller.
+void on_main_window_destroy(GtkWidget *w, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleMainWindowDestroy();
 }
 
+// Bridge GTK key press events to the setup controller.
+// @return controller handling result, or FALSE if no controller is present.
 gboolean on_main_window_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer data)
 {
     if (auto controller = global_setup_controller())
@@ -15,104 +18,121 @@ gboolean on_main_window_key_press_event(GtkWidget *widget, GdkEventKey *event, g
     return FALSE;
 }
 
-void on_btn_close_clicked(GtkButton* btn, gpointer user_data)
+// Bridge the close button clicked signal to the setup controller.
+void on_btn_close_clicked(GtkButton *btn, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleBtnClose();
 }
 
-void on_setting_toggled(GtkToggleButton* btn, gpointer user_data)
+// Bridge setting toggle events to the setup controller.
+void on_setting_toggled(GtkToggleButton *btn, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleSettingToggled(btn);
 }
 
-void on_setting_realize(GtkToggleButton* btn, gpointer user_data)
+// Bridge setting toggle realization events to the setup controller.
+void on_setting_realize(GtkToggleButton *btn, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleSettingRealize(btn);
 }
 
-void on_btn_macroedit_clicked(GtkButton* btn, gpointer user_data)
+// Bridge macro edit button clicks to the setup controller.
+void on_btn_macroedit_clicked(GtkButton *btn, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleMacroEdit();
 }
 
-void on_input_method_changed(GtkComboBox* cbb, gpointer user_data)
+// Bridge input method combo box changes to the setup controller.
+void on_input_method_changed(GtkComboBox *cbb, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleInputMethodChanged(cbb);
 }
 
-void on_input_method_realize(GtkComboBox* cbb, gpointer user_data)
+// Bridge input method combo box realization to the setup controller.
+void on_input_method_realize(GtkComboBox *cbb, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
-    controller->handleInputMethodRealize(cbb);
+        controller->handleInputMethodRealize(cbb);
 }
 
-void on_output_charset_changed(GtkComboBox* cbb, gpointer user_data)
+// Bridge output charset combo box changes to the setup controller.
+void on_output_charset_changed(GtkComboBox *cbb, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleOutputCharsetChanged(cbb);
 }
 
-void on_output_charset_realize(GtkComboBox* cbb, gpointer user_data)
+// Bridge output charset combo box realization to the setup controller.
+void on_output_charset_realize(GtkComboBox *cbb, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
-    controller->handleOutputCharsetRealize(cbb);
+        controller->handleOutputCharsetRealize(cbb);
 }
 
-gboolean on_macro_dialog_delete(GtkWidget* wid, GdkEvent* ev, gpointer user_data)
+// Bridge macro dialog delete events to the setup controller.
+// @return the controller delete handling result, or TRUE if no controller is present.
+gboolean on_macro_dialog_delete(GtkWidget *wid, GdkEvent *ev, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         return controller->handleMacroDialogDelete();
     return TRUE;
 }
 
-void macro_dialog_hide(GtkButton* btn, gpointer user_data)
+// Bridge hide dialog requests to the setup controller.
+void macro_dialog_hide(GtkButton *btn, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleMacroDialogHide();
 }
 
+// Bridge macro delete button clicks to the setup controller.
 void on_btn_macro_del_clicked(GtkButton *button, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleMacroDel();
 }
 
+// Bridge macro clear button clicks to the setup controller.
 void on_btn_macro_clear_clicked(GtkButton *button, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleMacroClear();
 }
 
+// Bridge macro import button clicks to the setup controller.
 void on_btn_macro_import_clicked(GtkButton *button, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleMacroImport();
 }
 
+// Bridge macro export button clicks to the setup controller.
 void on_btn_macro_export_clicked(GtkButton *button, gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleMacroExport();
 }
 
+// Bridge macro key cell edits to the setup controller.
 void on_cell_key_edited(GtkCellRendererText *celltext,
-                    const gchar *string_path,
-                    const gchar *newkey,
-                    gpointer user_data)
+                        const gchar *string_path,
+                        const gchar *newkey,
+                        gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleCellKeyEdited(celltext, string_path, newkey);
 }
 
+// Bridge macro value cell edits to the setup controller.
 void on_cell_value_edited(GtkCellRendererText *celltext,
-                     const gchar *string_path,
-                     const gchar *newvalue,
-                     gpointer user_data)
+                          const gchar *string_path,
+                          const gchar *newvalue,
+                          gpointer user_data)
 {
     if (auto controller = global_setup_controller())
         controller->handleCellValueEdited(celltext, string_path, newvalue);
